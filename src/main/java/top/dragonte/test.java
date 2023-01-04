1412d50d7cab9169d2a7b391e1749cad705ba7e5
Dragonte <78678132+Dragonte6967@users.noreply.github.com>
2023-01-04 07:49:49 +0000

    modified:   src/main/java/top/dragonte/test.java 	new file:   target/classes/plugin.yml 	new file:   target/classes/top/dragonte/test.class 	new file:   target/dragonte-1.0-SNAPSHOT.jar 	new file:   target/maven-archiver/pom.properties 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/createdFiles.lst 	new file:   target/maven-status/maven-compiler-plugin/compile/default-compile/inputFiles.lst 	new file:   target/original-dragonte-1.0-SNAPSHOT.jar

--- src/main/java/top/dragonte/test.java
+++ src/main/java/top/dragonte/test.java
@@ -7,6 +7,8 @@ public final class test extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
+        getLogger().info("插件加载成功");
+        getLogger().info("此插件为Dragonte6967在codespaces编写的第一个插件");
     }
 
     @Override
